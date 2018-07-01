53d679e249d03e4db7758e5168d7ed831f13525c
Jackie M <jnmccla@outlook.com>
2018-07-01 17:15:52 -0500

    Initial commit

---
+++ TechnoPagan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechnoPagan.Startup))]
+namespace TechnoPagan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
